6bd39479514fe2fe6587924ee6c439889822d5ab
P19010004\CAICA <he-hangrui@caica.jp>
2019-06-04 15:35:23 +0900

    課題Ex0110を作成しました。

---
+++ src/Ex0110.java
@@ -0,0 +1,32 @@
+package ojt_algorithm;
+
+import java.util.Scanner;
+
+public class Ex0110 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+		//キーボード入力欄
+		System.out.println("文字列1");
+
+		Scanner scan1 = new Scanner(System.in);
+		String str1 = scan1.next();
+
+		System.out.println("文字列2");
+
+		Scanner scan2 = new Scanner(System.in);
+		String str2 = scan2.next();
+
+		//文字列1と文字列2は同じだと「等しい」と出力、そうでないと比較して、辞書順で出力
+		if (str1.compareTo(str2) < 0) {
+			System.out.println(str1 + "  " + str2);
+		} else if (str1.compareTo(str2) == 0) {
+			System.out.println("２つの文字列は同じです -->" + str1);
+		} else {
+			System.out.println(str2 + "  " + str1);
+
+		}
+
+	}
+}
