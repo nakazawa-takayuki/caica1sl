625d3bde525a65a3db94944e606c9eea76f42aee
P19010024\CAICA <li-yu@caica.jp>
2019-06-07 11:39:30 +0900

    課題0110終了

--- src/liyu/Ex0110/Ex0110.java
+++ src/liyu/Ex0110/Ex0110.java
@@ -5,36 +5,29 @@ import java.util.Scanner;
 
 public class Ex0110 {
 
-	public static void main(String[] args)
-    {
+	public static void main(String[] args) {
 		System.out.println("文字列1を入力してください");
 		Scanner input1 = new Scanner(System.in);
 		String str1 = input1.nextLine();
-		String str11 = str1.replaceAll(" +","");
-
-
+		String str11 = str1.replaceAll(" +", "");
 
 		System.out.println("文字列2を入力してください");
 		Scanner input2 = new Scanner(System.in);
 		String str2 = input2.nextLine();
-		String str21 = str2.replaceAll(" +","");
+		String str21 = str2.replaceAll(" +", "");
 		input1.close();
 		input2.close();
 
+		if (str11.equals(str21)) {
+			System.out.println("２つの文字列は同じです" + "⇒" + str1);
+		} else {
+			// args is the list of guests
+			String[] args1 = { str11, str21 };
+			Arrays.sort(args1);
+			for (int i = 0; i < args1.length; i++)
+				System.out.println(args1[i]);
+		}
 
-		if (str11.equals(str21))
-		 {
-			 System.out.println("２つの文字列は同じです"+"⇒"+ str1);
-		 }
-		 else
-		 {
-        // args is the list of guests
-		String[] args1 = {str11,str21};
-        Arrays.sort(args1);
-        for(int i = 0; i < args1.length; i++)
-            System.out.println(args1[i]);
-		 }
-
-    }
+	}
 
 }
