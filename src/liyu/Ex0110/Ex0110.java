ebdc5b98ed023369ad99a210c9be033ba1126b20
P19010024\CAICA <li-yu@caica.jp>
2019-06-07 17:55:12 +0900

    no message

--- src/liyu/Ex0110/Ex0110.java
+++ src/liyu/Ex0110/Ex0110.java
@@ -9,23 +9,25 @@ public class Ex0110 {
 		System.out.println("文字列1を入力してください");
 		Scanner input1 = new Scanner(System.in);
 		String str1 = input1.nextLine();
-		String str11 = str1.replaceAll(" +", "");
 
 		System.out.println("文字列2を入力してください");
 		Scanner input2 = new Scanner(System.in);
 		String str2 = input2.nextLine();
-		String str21 = str2.replaceAll(" +", "");
 		input1.close();
 		input2.close();
 
-		if (str11.equals(str21)) {
+		if (str1.isEmpty()||str2.isEmpty()) {
+			System.out.println("エラー：文字列が検出出来ません");
+		}
+
+		else if (str1.equals(str2)) {
 			System.out.println("２つの文字列は同じです" + "⇒" + str1);
 		} else {
 			// args is the list of guests
-			String[] args1 = { str11, str21 };
-			Arrays.sort(args1);
-			for (int i = 0; i < args1.length; i++)
-				System.out.println(args1[i]);
+			String[] strArray = { str1, str2 };
+			Arrays.sort(strArray);
+			for (int i = 0; i < strArray.length; i++)
+				System.out.println(strArray[i]);
 		}
 
 	}
