3f16f48f8cd12e71d2ad6b22a81076c10607ca51
田畑 陽一朗 <tabata-yoichiro@caica.jp>
2019-06-10 15:50:51 +0900

    ソースコード修正

--- src/田畑陽一朗/Ex0200/Ex0200.java
+++ src/田畑陽一朗/Ex0200/Ex0200.java
@@ -2,6 +2,7 @@ package 田畑陽一朗.Ex0200;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 /**
  * キーボードから1つの整数値nを入力し、1からnまでの和を求め、結果を出力する。
  *
@@ -11,25 +12,23 @@ import java.util.Scanner;
 
 public class Ex0200 {
 
-	static int line1 = 0;
-
 	public static void main(String[] args) {
 
+		int line1 = 0;
 		Scanner scn = new Scanner(System.in);
-
 		try {
 			System.out.println("input number :");
 			line1 = scn.nextInt();
 		} catch (InputMismatchException e) {
 			System.out.println("整数を入力してください。");
+			return;
+		} finally {
+			scn.close();
 		}
-		scn.close();
-
-		int value = 0;
-		System.out.println("合計値 = " + totalValue(value));
+		System.out.println("合計値 = " + totalValue(line1));
 	}
 
-	private static int totalValue(int value) {
+	private static int totalValue(int line1) {
 		int sum = 0;
 		for (int i = 1; i <= line1; i++) {
 			sum += i;
