ec3dd3dca590d76db09f6f55d6854133844b35ca
金山涼香 <kanayama-suzuka@caica.jp>
2019-06-10 10:30:56 +0900

    Ex0160,Ex0200,Ex0210　作成

---
+++ src/kanayama/Ex0210/Ex0210.java
@@ -0,0 +1,12 @@
+package kanayama.Ex0210;
+
+public class Ex0210 {
+	 public static void main(String[] args) {
+	        for(int column = 1; column <= 9; column++) {
+	            for(int line = 1; line <= 9; line++) {
+	                System.out.printf("%02d", column * line);
+	            }
+	            System.out.println();
+	        }
+	    }
+}
