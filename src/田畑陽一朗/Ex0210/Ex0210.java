f0f54f336c3ff6255d5d2aa7129396a56cef080e
田畑 陽一朗 <tabata-yoichiro@caica.jp>
2019-06-10 14:18:48 +0900

    Ex0210 表示結果を見やすくしました

--- src/田畑陽一朗/Ex0210/Ex0210.java
+++ src/田畑陽一朗/Ex0210/Ex0210.java
@@ -1,4 +1,5 @@
 package 田畑陽一朗.Ex0210;
+
 /**
  * 九九の表を出力する。
  *
@@ -11,9 +12,9 @@ public class Ex0210 {
 
 		for (int i = 1; i <= 9; i++) {
 			for (int j = 1; j <= 9; j++) {
-				System.out.printf("%02d ", i * j);
-			}
-			System.out.println();
+				System.out.printf("%02d  ", i * j);
+			};
+			System.out.println(" ");
 		}
 	}
 }
