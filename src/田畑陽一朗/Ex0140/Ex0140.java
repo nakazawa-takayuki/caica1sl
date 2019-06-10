ecc744df4bb72c2a7c22883666a5195af0d4fdf1
田畑 陽一朗 <tabata-yoichiro@caica.jp>
2019-06-10 13:38:08 +0900

    Ex0140 修正

--- src/田畑陽一朗/Ex0140/Ex0140.java
+++ src/田畑陽一朗/Ex0140/Ex0140.java
@@ -7,23 +7,24 @@ import java.util.Scanner;
 /**
  * キーボードから2 つの整数値とコマンドを入力し、計算結果を出力する。
  * コマンドの意味は以下の通りとする。
+ * 0:足し算  1:引き算  2:掛け算  その他:割り算
  *
  * @author 田畑 陽一朗
  *
  */
 public class Ex0140 {
 
-	final static String sum = "0";
-	final static String subtraction = "1";
-	final static String multiplication = "2";
+	final static String add = "0";
+	final static String sub = "1";
+	final static String mul = "2";
 
 	public static void main(String[] args) {
 
 		Scanner scn = new Scanner(System.in);
 		Scanner scn2 = new Scanner(System.in);
 
-		int line1 = 0;
-		int line2 = 0;
+		double line1 = 0;
+		double line2 = 0;
 
 		try {
 			System.out.println("input number1:");
@@ -31,11 +32,8 @@ public class Ex0140 {
 			System.out.println("input number2:");
 			line2 = scn.nextInt();
 		} catch (InputMismatchException e) {
-			System.out.println("整数を入力してください。" + e);
-		} catch (NullPointerException e) {
-			System.out.println("値が入っていません。");
+			System.out.println("整数を入力してください。" );
 		}
-
 		System.out.println("input number3:");
 		String line3 = scn2.nextLine();
 		scn.close();
@@ -51,15 +49,15 @@ public class Ex0140 {
 	 * @param line1 入力値1
 	 * @param line2 入力値2
 	 * @param line3 コマンド
-	 * @return
+	 * @return 計算結果
 	 */
-	private static int calcResult(int line1, int line2, String line3) {
-		int result = 0;
-		if (line3.equals(sum)) {
+	private static double calcResult(double line1, double line2, String line3) {
+		double result = 0;
+		if (line3.equals(add)) {
 			result = line1 + line2;
-		} else if (line3.equals(subtraction)) {
+		} else if (line3.equals(sub)) {
 			result = line1 - line2;
-		} else if (line3.equals(multiplication)) {
+		} else if (line3.equals(mul)) {
 			result = line1 * line2;
 		} else {
 			result = line1 / line2;
