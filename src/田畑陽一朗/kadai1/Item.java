1bca6d5061dbb4162c7d81d989fd9b7dcf32db37
田畑 陽一朗 <tabata-yoichiro@caica.jp>
2019-06-14 09:34:09 +0900

    Ex0001　課題1 javadoc追加

--- src/田畑陽一朗/kadai1/Item.java
+++ src/田畑陽一朗/kadai1/Item.java
@@ -5,19 +5,35 @@ public class Item {
 	private String itemName; //商品名
 	private int price; //価格
 
+	/**
+	 * 商品の名前を取得
+	 * @return 商品名
+	 */
 	public String getItemName() {
 		return itemName;
 	}
 
+	/**
+	 * 価格を取得
+	 * @return 価格
+	 */
 	public int getPrice() {
 		return price;
 	}
 
+	/**
+	 * 商品名と価格を初期化する
+	 * @param itemName
+	 * @param price
+	 */
 	public Item(String itemName, int price) {
 		this.itemName = itemName;
 		this.price = price;
 	}
 
+	/**
+	 * オーバーライドして文字を出力
+	 */
 	@Override
 	public String toString() {
 		return itemName + ":" + Integer.toString(price) + "円";
--- src/田畑陽一朗/kadai1/Main.java
+++ src/田畑陽一朗/kadai1/Main.java
@@ -1,11 +1,18 @@
 package 田畑陽一朗.kadai1;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		Item item1 = new Item("マグロ",3000);
-		Item item2 = new Item("カニ",4500);
-		
+		/**
+		 *      商品名と価格の設定
+		 */
+		Item item1 = new Item("マグロ", 3000);
+		Item item2 = new Item("カニ", 4500);
+
+		/**
+		 *      商品名と価格を表示
+		 */
 		System.out.println(item1);
 		System.out.println(item2);
 	}
