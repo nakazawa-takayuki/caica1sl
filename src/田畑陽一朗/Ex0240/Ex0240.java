12ed20815203cbc5150e8ba212eb39a273715624
田畑 陽一朗 <tabata-yoichiro@caica.jp>
2019-06-20 17:18:41 +0900

    Ex0240 修正

--- src/田畑陽一朗/Ex0240/Ex0240.java
+++ src/田畑陽一朗/Ex0240/Ex0240.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 
 public class Ex0240 {
 
-	final static int multiplicationNumber = 9;
+	final static int MULTIPLICATION_NUMBER = 9;
 
 	public static void main(String[] args) {
 
 		Scanner scn = new Scanner(System.in);
 		int inputNum = 0;
+
 		try {
 			System.out.println("input number :");
 			inputNum = scn.nextInt();
+			if (inputNum < 10 || inputNum > 99) {
+				System.out.println("2桁の整数値を入力してください");
+				return;
+			}
 		} catch (InputMismatchException e) {
 			System.out.println("整数を入力してください。");
 			return;
@@ -26,8 +31,8 @@ public class Ex0240 {
 
 	public static void checkExistMultiplication(int inputNum) {
 		boolean judge = true;
-		for (int i = 0; i <= multiplicationNumber; i++) {
-			for (int j = 0; j <= multiplicationNumber; j++) {
+		for (int i = 0; i <= MULTIPLICATION_NUMBER; i++) {
+			for (int j = 0; j <= MULTIPLICATION_NUMBER; j++) {
 				if (inputNum == i * j) {
 					System.out.println("九九の表" + i + "*" + j + "にあります");
 					judge = false;
