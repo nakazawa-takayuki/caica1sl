ee3684239d3b5096ad7661f9bc77f317f32bee5f
田島蓮 <tajima-ren@caica.jp>
2019-06-20 10:09:54 +0900

    田島　Ex0235、Ex0240提出

---
+++ src/TajimaRen/Ex0235/Ex0235.java
@@ -0,0 +1,52 @@
+package TajimaRen.Ex0235;
+
+/**
+ * キーボードから入力された0～255 の範囲の10 進数整数値を2 進数と16 進数に変換して出力する。
+ * @author TajimaRen
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex0235 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("0～255の範囲の10進数：");
+
+		String str = br.readLine();
+		int inputNum;
+		String binaryNumber;		//2進数
+		String decimalNumber;		//16進数
+
+		try {
+			inputNum = Integer.parseInt(str);
+		}catch(NumberFormatException ex) {
+			System.out.println("数字を入力してください");
+			return;
+		}
+
+		//代入
+		binaryNumber = toBinaryNumber(inputNum);
+		decimalNumber = toDecimalNumber(inputNum);
+
+		//出力
+		if(inputNum > 255) {
+			System.out.println("0～255の範囲の数字を入力してください");
+		}else {
+			System.out.println("2進数の変換結果：" + String.format("%08d",Integer.parseInt(binaryNumber)));
+			System.out.println("16進数の変換結果：" + decimalNumber);
+		}
+	}
+
+	//2進数に変換
+	public static String toBinaryNumber(int inputNum) {
+		String bin = Integer.toBinaryString(inputNum);
+		return bin;
+	}
+
+	//16進数に変換
+	public static String toDecimalNumber(int inputNum) {
+		String hex = Integer.toHexString(inputNum);
+		return hex;
+	}
+}
---
+++ src/TajimaRen/Ex0240/Ex0240.java
@@ -0,0 +1,46 @@
+package TajimaRen.Ex0240;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 2 桁の数字を入力すると、九九の表に値があるかどうか出力する。
+ * @author TajimaRen
+ *
+ */
+public class Ex0240 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("input number：");
+
+		String str = br.readLine();
+		int inputNum;
+		boolean inTable = false;
+
+		try {
+			inputNum = Integer.parseInt(str);
+		}catch(NumberFormatException ex) {
+			System.out.println("数字を入力してください");
+			return;
+		}
+
+		if(inputNum >= 100) {
+			System.out.println("2桁の数字を入力してください");
+			return;
+		}
+
+		for(int i = 1; i <= 9;i++) {
+			for(int j = 1; j <= 9;j++) {
+				if(i*j == inputNum) {
+					System.out.println("九九の表（" + i + "*" + j + "）にあります");
+					inTable = true;
+				}
+			}
+		}
+		if(!inTable) {
+			System.out.println("九九の表にありません");
+		}
+	}
+}
