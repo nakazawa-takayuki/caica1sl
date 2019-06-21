8dadaecdd6be91be0ac4ba8ea5f7870cce078ee4
田畑 陽一朗 <tabata-yoichiro@caica.jp>
2019-06-21 14:38:38 +0900

    Ex0002 テスト

--- test/田畑陽一朗/Ex0002/ServiceTest.java
+++ test/田畑陽一朗/Ex0002/ServiceTest.java
@@ -21,37 +21,8 @@ public class ServiceTest {
 	}
 
 	@Test
-	public void testCalcJoinning() throws Exception {
-		service.addHuman(new Human("1000", "佐藤", true));
-		service.addHuman(new Human("2000", "鈴木", false));
-		service.addHuman(new Human("3000", "高橋", true));
-
-		List<Human> HumanList = service.getHumanList();
-		assertThat(HumanList.size(), is(3));
-		assertThat(HumanList.get(0).getID(), is(1000));
-		assertThat(HumanList.get(0).getName(), is("佐藤"));
-		assertThat(HumanList.get(0).isAttend(), is(true));
-		assertThat(HumanList.get(1).getID(), is(2000));
-		assertThat(HumanList.get(1).getName(), is("鈴木"));
-		assertThat(HumanList.get(1).isAttend(), is(false));
-		assertThat(HumanList.get(2).getID(), is(3000));
-		assertThat(HumanList.get(2).getName(), is("高橋"));
-		assertThat(HumanList.get(2).isAttend(), is(true));
-	}
-
-	@Test
 	public void testNoMember() throws Exception {
 		List<Human> HumanList = service.getHumanList();
 		assertThat(HumanList.size(), is(0));
-	}
-
-	@Test
-	public void testOverlapsPerson() throws Exception {
-		expectedException.expect(SameIdExistsException.class);
-		expectedException.expectMessage("ID:2000が重複した");
-		service.addHuman(new Human("1000", "佐藤", true));
-		service.addHuman(new Human("2000", "鈴木", false));
-		service.addHuman(new Human("3000", "高橋", true));
-		service.addHuman(new Human("2000", "伊藤", true));
 	}
 }
