821b41f131b816805e50f2811beb55ef3c05f4ff
田畑 陽一朗 <tabata-yoichiro@caica.jp>
2019-06-25 16:36:57 +0900

    Ex0256 提出修正

---
+++ src/田畑陽一朗/Ex0256/Ex0256.java
@@ -0,0 +1,35 @@
+package 田畑陽一朗.Ex0256;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+public class Ex0256 {
+
+	public static void main(String[] args) {
+
+		Scanner scn = new Scanner(System.in);
+		System.out.println("input date:");
+		String inputDate = scn.nextLine();
+		scn.close();
+
+		judgeInputDateFormat(inputDate);
+	}
+
+	public static void judgeInputDateFormat(String inputDate) {
+
+		boolean judge = false;
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		try {
+			dateFormat.setLenient(false);
+			dateFormat.parse(inputDate);
+		} catch (ParseException e) {
+			judge = true;
+		}
+		if (!judge) {
+			System.out.println("指定した形式です");
+		} else {
+			System.out.println("指定した形式ではありません");
+		}
+	}
+}
