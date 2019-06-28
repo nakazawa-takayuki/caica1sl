babaa3b4e0f9e7cfe8785011a4fdb18c35df6339
田島蓮 <tajima-ren@caica.jp>
2019-06-28 10:24:52 +0900

    田島　Ex0370修正

--- src/TajimaRen/Ex0370/Ex0370.java
+++ src/TajimaRen/Ex0370/Ex0370.java
@@ -16,62 +16,78 @@ import java.util.regex.Pattern;
  */
 public class Ex0370 {
 
-	static final int DIGIT = 4;
-	static final String GIVE_UP_NUMBER = "999";
+	static final Pattern PATTERN = Pattern.compile("^[0-9]{4}+$");	//4桁の半角数字か判定するパターン
+
+	static final String GIVE_UP_NUMBER = "999";						//ギブアップして答えを表示する数字
 
 	public static void main(String[] args) throws IOException {
-		String input;
-		String answer;
-		int count;
 
-		//引数が空、もしくは4桁でなかったときの処理
+		//引数が空だったときの処理
 		if(args.length == 0) {
 			System.out.println("引数が入っていません");
 			return;
-		}else if(args[0].length() < DIGIT || DIGIT < args[0].length()) {
-			System.out.println("引数が4桁ではありません");
+		}
+
+		//引数が4桁の半角数字ではなかったときの処理
+		Matcher matcherArgs = PATTERN.matcher(args[0]);
+		if(matcherArgs.find() == false) {
+			System.out.println("引数が4桁の半角数字ではありません");
 			return;
 		}
 
+		String input;
+		String answer;
+		int count = 0;
+
 		while(true) {
 			answer = args[0];
-			count = 1;
+			count++;
 
+			//入力
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("（" + count +"回目）予測数字を入力してください：");
+			System.out.println("（" + count + "回目）予測数字を入力してください：");
 			input = br.readLine();
 
-			//入力された文字列が数字ではなかったときの処理
-			Pattern pattern = Pattern.compile("^[0-9]*$");
-			Matcher matcher = pattern.matcher(input);
-			if (matcher.find() == false) {
-				System.out.println("数字を入力してください");
-				continue;
+			//答えを表示する「999」が入力されたときの処理と
+			//入力された文字列が4桁の半角数字ではなかったときの処理
+			Matcher matcherInput = PATTERN.matcher(input);
+
+			if(input.equals(GIVE_UP_NUMBER)) {
+				System.out.println("正解は" + answer + "でした。");
+				break;
+			}else if (matcherInput.find() == false) {
+				System.out.println("4桁の半角数字を入力してください");
+				break;
 			}
-
-			//4桁以下かつ999でない数字を入力されたときの処理
-			if (input.length() < DIGIT && !input.equals(GIVE_UP_NUMBER)) {
-				System.out.println("4桁の数字もしくは999を入力してください");
-				continue;
-
-			//4桁以上の数字を入力されたときの処理
-			} else if (DIGIT < input.length()) {
-				System.out.println("4桁の数字もしくは999を入力してください");
-				continue;
-			}
-
-			//MatchクラスとMatchクラスで求めたhitの数とbrowの数を持ってくる
-			Match match = new Match();
-			Match result = match.matchMethod(input,answer);
 
 			//出力
-			if (result.hit == DIGIT || input.equals(GIVE_UP_NUMBER)){
+			if (input.equals(answer)){
 				System.out.println("正解は" + answer + "でした。");
 				break;
 			} else {
-				System.out.println("ヒット：" + result.hit + "　ブロー：" + result.brow);
-				count++;
+				hitAndBrow(input,answer);
 			}
 		}
+	}
+
+	public static void hitAndBrow(String input,String answer) {
+
+		int hit ;
+		int brow ;
+		String[] inputArrays = input.split("");
+		String[] answerArrays = answer.split("");
+		hit = 0;
+		brow = 0;
+
+		for (int i = 0; i < inputArrays.length; i++) {
+			for (int j = 0; j < answerArrays.length; j++) {
+				if (i == j && inputArrays[i].equals(answerArrays[j])) {
+					hit++;
+				} else if (inputArrays[i].equals(answerArrays[j])) {
+					brow++;
+				}
+			}
+		}
+		System.out.println("ヒット：" + hit + "　ブロー：" + brow);
 	}
 }
